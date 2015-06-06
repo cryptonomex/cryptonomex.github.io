9106bf846a5a4781040952fdb540e5ca7c535d5c
cass <cass@bitshares.org>
2015-06-06 18:52:53 +0200

    minor design tweaks
    - contact.html etc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+   $('.modal-trigger').leanModal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        in_duration: 300, // Transition in duration
+        out_duration: 200, // Transition out duration
+   
+      }
+    );
+  });
+          
+
+
+
+
 jQuery(document).ready(function($){
 	//set animation timing
 	var animationDelay = 2500,
